f67ae7720e8b3d2f757a1f3d0de7740fe9d86fe5
harish.dhami <harish.dhami@naukri.com>
2016-11-16 10:12:30 +0530

    a bug fix regarding directory structure

--- components/getFileType.js
+++ components/getFileType.js
@@ -1,8 +1,9 @@
 module.exports = function(grunt) { /*this function gets the file type of files*/
     return function(dirname,rootDirArr,fileType,regex) {
         for (var i = 0; i < rootDirArr.length; i++) {
-            regex = new RegExp(rootDirArr[i]);
-            if (dirname.match(regex)) {
+            //regex = new RegExp(rootDirArr[i]);
+            regex = rootDirArr[i];
+            if (dirname==regex) {
                 fileType = rootDirArr[i];
                 break;
             }
